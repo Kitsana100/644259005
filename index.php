94141fd8d8f09187988999501167361b12cc4dcb
Kitsana100 <Kitsanahora13@gmail.com>
2023-03-16 11:52:20 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Siamphone</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Spec Phone</h1>
+<div class="conphone">
+<div class="p1">
+    <img src="./img/p1.png" class="imgp1">
+        <h1>Iphone 12 Pro Max</h1>
+        <p class="price">฿33,290</p>
+    <button type="button" class="btn btn-success">Buy</button>
+</div>
+
+<div class="p1">
+    <img src="./img/p2.png" class="imgp2">
+        <h1>Iphone 13 Pro Max</h1>
+        <p class="price">฿44,000</p>
+    <button type="button" class="btn btn-success">Buy</button>
+</div>
+
+<div class="p1">
+    <img src="./img/p3.png" class="imgp3">
+        <h1>Iphone 14 Pro Max</h1>
+        <p class="price">฿62,400</p>
+    <button type="button" class="btn btn-success">Buy</button>
+</div>
+</div>
+</body>
+</html>
